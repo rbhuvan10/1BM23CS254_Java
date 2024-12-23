904282c481c3e969ca4ecb7f98d21ee3a8637152
rbhuvan10 <rbhuvan.cs23@bmsce.ac.in>
2024-12-23 20:48:34 +0530

    Create Lab2.java

---
+++ Lab2.java
@@ -0,0 +1,90 @@
+import java.util.Scanner;
+
+class Book {
+    private String name;
+    private String author;
+    private double price;
+    private int num_pages;
+
+    public Book(String name, String author, double price, int num_pages) {
+        this.name = name;
+        this.author = author;
+        this.price = price;
+        this.num_pages = num_pages;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public int getNumPages() {
+        return num_pages;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public void setNumPages(int num_pages) {
+        this.num_pages = num_pages;
+    }
+
+    @Override
+    public String toString() {
+        return "Book Name: " + name + "\nAuthor: " + author + "\nPrice: " + price + "\nNumber of Pages: " + num_pages;
+    }
+}
+
+public class BookDemo {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter number of books: ");
+        int n = scanner.nextInt();
+        scanner.nextLine();  
+
+
+        Book[] books = new Book[n];
+
+
+        for (int i = 0; i < n; i++) {
+            System.out.println("Enter details for book " + (i + 1) + ":");
+            System.out.print("Name: ");
+            String name = scanner.nextLine();
+            System.out.print("Author: ");
+            String author = scanner.nextLine();
+            System.out.print("Price: ");
+            double price = scanner.nextDouble();
+            System.out.print("Number of Pages: ");
+            int num_pages = scanner.nextInt();
+            scanner.nextLine();  
+
+
+            books[i] = new Book(name, author, price, num_pages);
+        }
+
+
+        for (int i = 0; i < n; i++) {
+            System.out.println("\nDetails of Book " + (i + 1) + ":");
+            System.out.println(books[i].toString());
+        }
+
+        scanner.close();
+    }
+}
